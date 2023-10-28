b621d4293c0b98d72e17ce1db2804554b4048075
Renat Shabanov <multizloy@gmail.com>
2023-10-28 23:16:30 +0300

    created custom login form

--- football_club/registration/urls.py
+++ football_club/registration/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
-from .views import RegisterView
+
+from .views import CreateRegisterView
+
+# from .views import RegisterView
 from django.contrib.auth.views import (
     LoginView,
     LogoutView,
@@ -11,7 +14,8 @@ app_name = "registration"
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("register/", RegisterView.as_view(), name="register"),
+    # path("register/", RegisterView.as_view(), name="register"),
+    path("register/", CreateRegisterView.as_view(), name="register"),
     # path("login/", views.login, name="login"),
     # path("logout/", views.logout, name="logout"),
 ]
--- football_club/registration/views.py
+++ football_club/registration/views.py
@@ -1,52 +1,23 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
+
+from .forms import MyUserCreationForm
 
 # Create your views here.
 
 
-class RegisterView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
+# class RegisterView(generic.CreateView):
+#     form_class = UserCreationForm
+#     template_name = "registration/register.html"
+#     success_url = reverse_lazy("registration:login")
+
+
+class CreateRegisterView(generic.CreateView):
+    form_class = MyUserCreationForm
     template_name = "registration/register.html"
 
-
-# def registerPage(request):
-#     form = UserCreationForm()
-
-#     if request.method == "POST":
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect("login")
-#         else:
-#             return redirect("registration/register.html")
-#     else:
-#         context = {"form": form}
-#         return render(request, "registration/register.html", context)
-
-
-# def register(request):
-#     if request.method == "POST":
-#         username = request.POST["username"]
-#         password = request.POST["password"]
-#         user = authenticate(request, user)
-#         if user is not None:
-#             login(request, user)
-#             return redirect("main/home.html")
-#         else:
-#             return redirect("registration/register.html")
-#     else:
-#         return render(request, "registration/register.html")
-
-
-# def login(request):
-#     context = {}
-#     return render(request, "registration/register.html", context)
-
-
-# def logout(request):
-#     logout(request)
-#     return redirect("main/home.html")
+    success_url = reverse_lazy("registration:login")

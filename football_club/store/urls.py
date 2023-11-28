d50f010476cbd0f571a1dc038115b4d4145a83d6
Renat Shabanov <multizloy@gmail.com>
2023-11-28 17:12:32 +0300

    добавил страницы для категорий товаров
    скорее всего есть лучший спопсоб отображений товаров по категориям

--- football_club/store/urls.py
+++ football_club/store/urls.py
@@ -6,4 +6,9 @@ app_name = "store"
 
 urlpatterns = [
     path("", Item_List.as_view(), name="item-list"),
+    path("category-shorts/", Category_Shorts.as_view(), name="category-shorts"),
+    path("category-hats/", Category_Hats.as_view(), name="category-hats"),
+    path("category-boots/", Category_Boots.as_view(), name="category-boots"),
+    path("category-tshirts/", Category_Tshirts.as_view(), name="category-tshirts"),
+    path("category-skarfs/", Category_Skarfs.as_view(), name="category-skarfs"),
 ]
--- football_club/store/views.py
+++ football_club/store/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
 
-from store.models import Item
+from store.models import Item, Category
 
 # Create your views here.
-
 
 
 class Item_List(generic.ListView):
@@ -22,3 +21,68 @@ class Item_List(generic.ListView):
         #     username=me, email=me, first_name=me, last_name=me
         # )
         return Item.objects.all()
+
+
+class Category_Shorts(generic.ListView):
+    template_name = "store/category_shorts.html"
+    form_class = Item
+    context_object_name = "shorts"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self):
+        return Item.objects.filter(category_id=5)
+
+
+class Category_Hats(generic.ListView):
+    template_name = "store/category_hats.html"
+    form_class = Item
+    context_object_name = "hats"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self):
+        return Item.objects.filter(category_id=6)
+
+
+class Category_Boots(generic.ListView):
+    template_name = "store/category_boots.html"
+    form_class = Item
+    context_object_name = "boots"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self):
+        return Item.objects.filter(category_id=4)
+
+
+class Category_Tshirts(generic.ListView):
+    template_name = "store/category_tshirts.html"
+    form_class = Item
+    context_object_name = "tshirts"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self):
+        return Item.objects.filter(category_id=2)
+
+
+class Category_Skarfs(generic.ListView):
+    template_name = "store/category_skarfs.html"
+    form_class = Item
+    context_object_name = "skarfs"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self):
+        return Item.objects.filter(category_id=3)

9c81cf3c36c029563a6fe1b22503ed84877e8c8d
Renat Shabanov <multizloy@gmail.com>
2024-01-05 19:28:52 +0300

    rf

--- django-rf/drf_course/urls.py
+++ django-rf/drf_course/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.urls import path
 from django.contrib import admin
+from core import views as core_views
 from rest_framework import routers
+
 
 router = routers.DefaultRouter()
 
@@ -24,4 +26,5 @@ urlpatterns = router.urls
 
 urlpatterns += [
     path("admin/", admin.site.urls),
+    path("contact/", core_views.ContactAPIView.as_view()),
 ]
